c31d279274da68f83ad0b7daddf595aae7174bd3
Ubuntu <ubuntu@ip-172-31-82-118.ec2.internal>
2021-12-16 03:13:46 +0000

    final

---
+++ Inheritance.java
@@ -0,0 +1,15 @@
+/*
+* This class creates an integer stack
+*
+* @author  Haokai
+* @version 1.0
+* @since   2021-12-01
+*
+*/
+
+/**
+* This is the standard "lock" program.
+*/
+public class Inheritance extends Throwing {
+}
+
